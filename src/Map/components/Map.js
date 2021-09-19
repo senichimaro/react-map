fda196c3e6725ef865150359df622896033eae89
senichimaro <senichimaro@gmail.com>
2021-09-20 00:38:52 +0200

    init local repo

--- src/Map/components/Map.js
+++ src/Map/components/Map.js
@@ -1,19 +1,60 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import React, {
+  useState
+}
+from 'react';
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+}
+from 'react-leaflet';
 // import 'leaflet/dist/leaflet.css';
+import './Map.css'
+
+
+
+
+
+function LocationMarker() {
+  const [position, setPosition] = useState(null);
+
+
+  const map = useMapEvents({
+    click(e) {
+        // map.locate();
+        // console.log(e);
+        setPosition(e.latlng);
+      },
+      locationfound(event) {
+        setPosition(event.latlng);
+        map.flyTo(event.latlng, map.getZoom());
+      },
+  });
+
+  return position === null
+  ? null
+  : (
+      <Marker
+        position={ position }
+      >
+        <Popup>You are Here!</Popup>
+      </Marker>
+  )
+}
 
 const Map = () => {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      style={{height: '30rem'}}
+    >
+    <TileLayer
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <LocationMarker />
     </MapContainer>
   );
 };
